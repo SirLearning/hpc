1b5f2396c902bf725ea77b33f60ad7400d93dbe7
Zheng_Da <zhengnda@outlook.com>
2025-06-20 18:02:13 +0800

    learn cpp

---
+++ practice/cpp/ch02.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Zheng on 2025/6/11.
+//
+
+#include "ch02.h"
---
+++ practice/cpp/ch02.h
@@ -0,0 +1,28 @@
+//
+// Created by Zheng on 2025/6/11.
+//
+
+#ifndef CH02_H
+#define CH02_H
+#define PI 3.1415926
+#include <iostream>
+using namespace std;
+
+class ch02 {
+    typedef unsigned int Unit;
+    const double PI1 = 3.1415926;
+    int a = 0;
+    int b = a + 1;
+    int i;
+    double d;
+    int main() {
+        cin >> i;
+        cin >> d;
+        return i + d;
+    }
+
+};
+
+
+
+#endif //CH02_H
